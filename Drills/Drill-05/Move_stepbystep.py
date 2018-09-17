a59326270c2aed56048bed8efbe044d1db3b1d4a
kth4540 <harypoteck@naver.com>
2018-09-17 20:21:03 +0900

    Drill-05 step 2
    함수 이름 변경 및 Move_132_243_to_535_470()함수 작동 확인

--- Drills/Drill-05/Move_stepbystep.py
+++ Drills/Drill-05/Move_stepbystep.py
@@ -4,7 +4,7 @@ grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
 
 
-def Move_1():
+def Move_203_535_to_132_243():
     x,y=203,535
     frame=0
     count=0
@@ -17,40 +17,49 @@ def Move_1():
         y=y-(535-243)//20
         count=count+1
         delay(0.05)
-def Move_2():
+def Move_132_243_to_535_470():
+    x,y=132,243
+    frame=0
+    count=0
+    while(count<20):
+        clear_canvas()
+        character.clip_draw(frame * 100, 100, 100, 100, x, y)
+        update_canvas()
+        frame=(frame+1)%8
+        x=x-(132-535)//20
+        y=y-(243-470)//20
+        count=count+1
+        delay(0.05)
+def Move_535_470_to_477_203():
     pass
-def Move_3():
+def Move_477_203_to_715_136():
     pass
-def Move_4():
+def Move_715_136_to_316_225():
     pass
-def Move_5():
+def Move_316_225_to_510_92():
     pass
-def Move_6():
+def Move_510_92_to_692_518():
     pass
-def Move_7():
+def Move_692_518_to_682_336():
     pass
-def Move_8():
+def Move_682_336_to_712_349():
     pass
-def Move_9():
-    pass
-def Move_10():
-    pass
+
 def Go_Back():
     pass
 
 
 
 while(True):
-    #Move_1()
-    Move_2()
-    Move_3()
-    Move_4()
-    Move_5()
-    Move_6()
-    Move_7()
-    Move_8()
-    Move_9()
-    Move_10()
+    #Move_203_535_to_132_243()
+    Move_132_243_to_535_470()
+    Move_535_470_to_477_203()
+    Move_477_203_to_715_136()
+    Move_715_136_to_316_225()
+    Move_316_225_to_510_92()
+    Move_510_92_to_692_518()
+    Move_692_518_to_682_336()
+    Move_682_336_to_712_349()
     Go_Back()
 close_canvas()
 
