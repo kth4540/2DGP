523e8a31fee512c8c79720c0431edb5e3197b81a
kth4540 <harypoteck@naver.com>
2018-09-17 20:28:29 +0900

    Drill-05 step 7
    Move_510_92_to_692_518() 함수 작동 확인

--- Drills/Drill-05/Move_stepbystep.py
+++ Drills/Drill-05/Move_stepbystep.py
@@ -83,7 +83,18 @@ def Move_316_225_to_510_92():
         count = count + 1
         delay(0.05)
 def Move_510_92_to_692_518():
-    pass
+    x, y = 510, 92
+    frame = 0
+    count = 0
+    while (count < 20):
+        clear_canvas()
+        character.clip_draw(frame * 100, 100, 100, 100, x, y)
+        update_canvas()
+        frame = (frame + 1) % 8
+        x = x - (510 - 692) // 20
+        y = y - (92 - 518) // 20
+        count = count + 1
+        delay(0.05)
 def Move_692_518_to_682_336():
     pass
 def Move_682_336_to_712_349():
