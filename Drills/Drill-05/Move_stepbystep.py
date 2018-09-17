493e4f7bebe0a5c75c6a8f2817eb606c8d522eb8
kth4540 <harypoteck@naver.com>
2018-09-17 20:02:46 +0900

    Drill-05 재시작, 기반
    에러메세지 발생 이유를 못찾아서 새로 제작,

--- Drills/Drill-05/Move_stepbystep.py
+++ Drills/Drill-05/Move_stepbystep.py
@@ -3,20 +3,43 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
 
-chx,chy=203,535
-frame=0
-def Move(x,y):
-    if(x-chx>0):
-        Move_left()
-    elif(x-chx<0):
-        Move_right()
 
-def Move_left():
+def Move_1():
     pass
-def Move_right():
+def Move_2():
     pass
+def Move_3():
+    pass
+def Move_4():
+    pass
+def Move_5():
+    pass
+def Move_6():
+    pass
+def Move_7():
+    pass
+def Move_8():
+    pass
+def Move_9():
+    pass
+def Move_10():
+    pass
+def Go_Back():
+    pass
+
+
 
 while(True):
-    Move(chx,chy)
+    Move_1()
+    Move_2()
+    Move_3()
+    Move_4()
+    Move_5()
+    Move_6()
+    Move_7()
+    Move_8()
+    Move_9()
+    Move_10()
+    Go_Back()
 close_canvas()
 
