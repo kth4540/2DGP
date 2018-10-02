ad5fef9e8f78b45854a33082f4673ae8d3bec61b
kth4540 <harypoteck@naver.com>
2018-10-02 16:44:08 +0900

    Drill 07 기본
    이미지 로드 및 함수 생성

---
+++ Drills/Drill-07/kpu_line.py
@@ -0,0 +1,17 @@
+from pico2d import *
+import turtle
+import random
+width,height=1280,1024
+open_canvas(width,height)
+
+def move(p1,p2):
+    pass
+
+map=load_image('KPU_GROUND.png')
+chararter=load_image('animation_sheet.png')
+
+
+
+
+
+close_canvas()
