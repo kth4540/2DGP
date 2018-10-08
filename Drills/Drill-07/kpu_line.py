2a25e27f7377214ba71ade5ed3e6db35d8031153
kth4540 <harypoteck@naver.com>
2018-10-08 14:41:19 +0900

    drill-08 1차
    p1~p2 곡선

--- Drills/Drill-07/kpu_line.py
+++ Drills/Drill-07/kpu_line.py
@@ -10,16 +10,22 @@ points=[(random.randint(200,1000),random.randint(200,800))for i in range(size)]
 n=1
 x=0
 y=0
+frame=0
 def move(p1,p2):
     global x
     global y
+    global frame
     for i in range(0,100,2):
         t=i/100
         x = (1 - t) * p1[0] + t * p2[0]
         y = (1 - t) * p1[1] + t * p2[1]
         clear_canvas()
         map.draw(width/2,height/2)
-        chararter.clip_draw(0,0,100,100,x,y)
+        if(p1[0]>p2[0]):
+            chararter.clip_draw(frame*100,0,100,100,x,y)
+        elif(p1[0]<p2[0]):
+            chararter.clip_draw(frame * 100, 100, 100, 100, x, y)
+        frame=(frame+1)%8
         update_canvas()
         delay(0.05)
         get_events()
