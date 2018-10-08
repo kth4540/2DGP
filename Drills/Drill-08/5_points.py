4b5e78eff4c3e0c7bcf57f5e523faedfd98b601c
kth4540 <harypoteck@naver.com>
2018-10-08 14:47:44 +0900

    drill-08 최종 (1번)
    2017182011 김태혁
    https://youtu.be/62CFg8ewHhg
    총 커밋 5회

--- Drills/Drill-08/5_points.py
+++ Drills/Drill-08/5_points.py
@@ -144,7 +144,15 @@ def draw_curve_5_points(p1, p2, p3, p4, p5):
         draw_point((x, y))
     draw_point(p5)
 
-    
+    # draw p4-p5
+    for i in range(0, 100, 2):
+        t = i / 100
+        x = ((-t ** 3 + 2 * t ** 2 - t) * p4[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p5[0] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * p1[0] + (t ** 3 - t ** 2) * p2[0]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * p4[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p5[1] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * p1[1] + (t ** 3 - t ** 2) * p2[1]) / 2
+        draw_point((x, y))
+    draw_point(p1)
 
 
 
@@ -152,6 +160,7 @@ def draw_curve_5_points(p1, p2, p3, p4, p5):
 
 prepare_turtle_canvas()
 
-draw_curve_5_points((-300, 200), (400, 350), (300, -300),(100,100), (-200, -200))
+while(True):
+    draw_curve_5_points((-300, 200), (400, 350), (300, -300),(100,100), (-200, -200))
 
 turtle.done()
