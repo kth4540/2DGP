0663cb290cfc130dab59e486c50e4faf91e3b75a
kth4540 <harypoteck@naver.com>
2018-10-08 15:15:34 +0900

    drill 08 8차
    p9~p10 이동

--- Drills/Drill-08/10_points_ch.py
+++ Drills/Drill-08/10_points_ch.py
@@ -156,6 +156,24 @@ def draw_curve_10_points(p1, p2, p3, p4, p5,p6,p7,p8,p9,p10):
         get_events()
         delay(0.05)
 
+# draw p9-p10
+    for i in range(0, 100, 2):
+        clear_canvas()
+        map.draw(width/2,height/2)
+        t = i / 100
+        x = ((-t ** 3 + 2 * t ** 2 - t) * p8[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p9[0] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * p10[0] + (t ** 3 - t ** 2) * p1[0]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * p8[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p9[1] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * p10[1] + (t ** 3 - t ** 2) * p1[1]) / 2
+        if (x > p9[0]):
+            chararter.clip_draw(100 * frame, 100, 100, 100, x, y)
+        elif (x < p9[0]):
+            chararter.clip_draw(100 * frame, 0, 100, 100, x, y)
+        frame=(frame+1)%8
+        update_canvas()
+        get_events()
+        delay(0.05)
+
 
 
 
