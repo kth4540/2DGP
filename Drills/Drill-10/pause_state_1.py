57664ce561c220a8cde50c368eff2d8e290a0779
kth4540 <harypoteck@naver.com>
2018-10-15 20:55:37 +0900

    drill-10 4차
    handle_events 작성

--- Drills/Drill-10/pause_state_1.py
+++ Drills/Drill-10/pause_state_1.py
@@ -19,7 +19,14 @@ def exit():
     del(pause)
 
 def handle_events():
-    pass
+    events=get_events()
+    for event in events:
+        if event.type==SDL_QUIT:
+            game_framework.quit()
+        elif event.type==SDL_KEYDOWN and event.key==SDLK_ESCAPE:
+            game_framework.change_state(title_state)
+        elif event.type==SDL_KEYDOWN and event.key==SDLK_p:
+            game_framework.pop_state()
 
 def draw():
     pass
