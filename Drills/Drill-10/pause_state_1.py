15caf08ac112e38a2023f80b8123be9b5398bdee
kth4540 <harypoteck@naver.com>
2018-10-15 20:33:37 +0900

    drill-10 2차
    프레임워크 작성

--- Drills/Drill-10/pause_state_1.py
+++ Drills/Drill-10/pause_state_1.py
@@ -8,3 +8,24 @@ class Pause:
         self.image=load_image('pause.png')
     def draw(self):
         self.image.draw(400,300)
+
+def enter():
+    pass
+
+def exit():
+    pass
+
+def handle_events():
+    pass
+
+def draw():
+    pass
+
+def update():
+    pass
+
+def pause():
+    pass
+
+def resume():
+    pass
