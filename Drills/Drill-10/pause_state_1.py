2498ed2402eb9a202832703e518a05095aeb21aa
kth4540 <harypoteck@naver.com>
2018-10-15 20:30:53 +0900

    drill-10 1차
    pause_state_1 생성 및 import

---
+++ Drills/Drill-10/pause_state_1.py
@@ -0,0 +1,10 @@
+import game_framework
+import main_state
+from pico2d import*
+pause=None
+
+class Pause:
+    def __init__(self):
+        self.image=load_image('pause.png')
+    def draw(self):
+        self.image.draw(400,300)
---
+++ Drills/Drill-10/simple_module.py
@@ -0,0 +1,4 @@
+def main():
+    print("The value of __name__ is '" + __name__ + "'.")
+if __name__=='__main__':
+    main()
