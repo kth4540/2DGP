95fca72de1529786ea0a1048c85366fb2911bc3e
kth4540 <harypoteck@naver.com>
2018-10-15 20:53:05 +0900

    drill-10 3차
    enter , exit 작성

--- Drills/Drill-10/pause_state_1.py
+++ Drills/Drill-10/pause_state_1.py
@@ -1,8 +1,9 @@
 import game_framework
 import main_state
+import title_state
 from pico2d import*
 pause=None
-
+image=None
 class Pause:
     def __init__(self):
         self.image=load_image('pause.png')
@@ -10,10 +11,12 @@ class Pause:
         self.image.draw(400,300)
 
 def enter():
-    pass
+    global pause
+    pause=Pause()
 
 def exit():
-    pass
+    global pause
+    del(pause)
 
 def handle_events():
     pass
