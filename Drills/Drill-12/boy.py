3caf3640ff34a5911c792d61942109f491433677
kth4540 <harypoteck@naver.com>
2018-10-30 18:44:30 +0900

    Drill-12 2차
    sleep상태에 들어가면 귀신이 회전(속도 조정X)

--- Drills/Drill-12/boy.py
+++ Drills/Drill-12/boy.py
@@ -56,7 +56,7 @@ class IdleState:
     @staticmethod
     def do(boy):
         boy.frame = (boy.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 8
-        if get_time()-boy.timer>=9.5:
+        if get_time()-boy.timer>=2.5:
             boy.add_event(SLEEP_TIMER)
 
     @staticmethod
--- Drills/Drill-12/ghost.py
+++ Drills/Drill-12/ghost.py
@@ -1,7 +1,11 @@
 from pico2d import *
+import math
+import time
 import game_world
 import main_state
 import boy
+
+rad_time=0.0
 
 class Ghost:
     image = None
@@ -10,13 +14,22 @@ class Ghost:
         if Ghost.image == None:
             Ghost.image = load_image('ball21x21.png')
         self.x, self.y = main_state.boy.x,main_state.boy.y
-
+        self.rad=-90
     def draw(self):
         if(main_state.boy.cur_state==boy.SleepState):
             self.image.draw(self.x, self.y)
 
     def update(self):
-        pass
+        global rad_time
+
+        if (main_state.boy.cur_state == boy.IdleState):
+            self.x, self.y = main_state.boy.x, main_state.boy.y
+        elif(main_state.boy.cur_state==boy.SleepState):
+            self.x=main_state.boy.x+300*math.cos(math.pi/180*self.rad)
+            self.y =390+ 300 * math.sin(math.pi / 180 * self.rad)
+            #if((get_time()-rad_time)):
+            self.rad+=1
+
 
         if self.x < 25 or self.x > 1600 - 25:
             game_world.remove_object(self)
