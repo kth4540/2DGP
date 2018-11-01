c3bd93d0be98911b278085967c3c2ad31e113518
kth4540 <harypoteck@naver.com>
2018-11-01 22:21:11 +0900

    2DGP 프로젝트 8차
    캐릭터 컨트롤러로 수정( 충돌체크 미구현)

--- 2DGP 프로젝트/CADENCE.py
+++ 2DGP 프로젝트/CADENCE.py
@@ -1,23 +1,150 @@
 from pico2d import*
 import game_framework
-
+import main_state
+# 프레임
 TIME_PER_ACTION = 1.0
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
 FRAMES_PER_ACTION = 8
+
+#cadence event
+
+RIGHT_ON, LEFT_ON, UP_ON, DOWN_ON,RIGHT_OFF,LEFT_OFF,UP_OFF,DOWN_OFF = range(8)
+
+key_event_table = {
+    (SDL_KEYDOWN, SDLK_RIGHT): RIGHT_ON,
+    (SDL_KEYDOWN, SDLK_LEFT): LEFT_ON,
+    (SDL_KEYDOWN, SDLK_UP): UP_ON,
+    (SDL_KEYDOWN, SDLK_DOWN): DOWN_ON,
+    (SDL_KEYUP, SDLK_RIGHT): RIGHT_OFF,
+    (SDL_KEYUP, SDLK_LEFT): LEFT_OFF,
+    (SDL_KEYUP, SDLK_UP): UP_OFF,
+    (SDL_KEYUP, SDLK_DOWN): DOWN_OFF
+}
+
+class IdleState:
+    @staticmethod
+    def enter(cadence,event):
+        if event==RIGHT_ON:
+            cadence.vertical+=1
+        elif event==LEFT_ON:
+            cadence.vertical-=1
+        elif event==UP_ON:
+            cadence.horizon+=1
+        elif event==DOWN_ON:
+            cadence.horizon-=1
+        elif event==RIGHT_OFF:
+            cadence.vertical-=1
+        elif event==LEFT_OFF:
+            cadence.vertical+=1
+        elif event == UP_OFF:
+            cadence.horizon -= 1
+        elif event == DOWN_OFF:
+            cadence.horizon += 1
+
+    @staticmethod
+    def exit(cadence,event):
+        pass
+
+
+
+    @staticmethod
+    def do(cadence):
+        cadence.frame = (cadence.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 4
+        cadence.rhythm+=400*115/60*game_framework.frame_time
+        if cadence.rhythm>=400:
+            cadence.act=False
+            cadence.rhythm=0
+
+    @staticmethod
+    def draw(cadence):
+        cadence.image.clip_draw(int(cadence.frame) * 24, 24, 24, 24, cadence.x, cadence.y)
+
+class RunState:
+    @staticmethod
+    def enter(cadence,event):
+        if event==RIGHT_ON:
+            cadence.vertical+=1
+        elif event==LEFT_ON:
+            cadence.vertical-=1
+        elif event==UP_ON:
+            cadence.horizon+=1
+        elif event==DOWN_ON:
+            cadence.horizon-=1
+        elif event==RIGHT_OFF:
+            cadence.vertical-=1
+        elif event==LEFT_OFF:
+            cadence.vertical+=1
+        elif event == UP_OFF:
+            cadence.horizon -= 1
+        elif event == DOWN_OFF:
+            cadence.horizon += 1
+
+    @staticmethod
+    def exit(cadence,event):
+        pass
+
+    @staticmethod
+    def do(cadence):
+        cadence.frame = (cadence.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 4
+        if cadence.act==False:
+            cadence.x += cadence.vertical * 24
+            cadence.y += cadence.horizon * 24
+            cadence.act=True
+        cadence.rhythm += 400 * 115 / 60 * game_framework.frame_time
+        if cadence.rhythm >= 400:
+            cadence.act = False
+            cadence.rhythm = 0
+
+
+
+
+    @staticmethod
+    def draw(cadence):
+        cadence.image.clip_draw(int(cadence.frame) * 24, 24, 24, 24, cadence.x, cadence.y)
+
+
+next_state_table={
+    IdleState:{RIGHT_ON:RunState,LEFT_ON:RunState,UP_ON:RunState,DOWN_ON:RunState,
+               RIGHT_OFF:IdleState, LEFT_OFF:IdleState, UP_OFF:IdleState,DOWN_OFF:IdleState},
+    RunState:{RIGHT_ON:RunState,LEFT_ON:RunState,UP_ON:RunState,DOWN_ON:RunState,
+               RIGHT_OFF:IdleState, LEFT_OFF:IdleState, UP_OFF:IdleState,DOWN_OFF:IdleState}
+}
+
 
 class Cadence:
     def __init__(self):
         self.x, self.y = 104+24, 500-24
         self.frame = 0
+        self.vertical=0
+        self.horizon=0
         self.image = load_image('clone.png')
         self.attack_sound=load_wav('attack.wav')
+        self.rhythm=0
+        self.act=True
+        self.event_que=[]
+        self.cur_state=IdleState
+        self.cur_state.enter(self,None)
+
+    def add_event(self, event):
+        self.event_que.insert(0, event)
 
     def update(self):
-        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 4
+        self.cur_state.do(self)
+        if len(self.event_que) > 0:
+            event = self.event_que.pop()
+            self.cur_state.exit(self, event)
+            self.cur_state = next_state_table[self.cur_state][event]
+            self.cur_state.enter(self, event)
 
-    def attack(self):
-        self.attack_sound.set_volume(500)
-        self.attack_sound.play()
+    def tmp(self):
+        print("HELLO")
 
     def draw(self):
-        self.image.clip_draw(int(self.frame) * 24, 24, 24, 24, self.x, self.y)
+        self.cur_state.draw(self)
+
+    def handle_event(self, event):
+        if (event.type, event.key) in key_event_table:
+            key_event = key_event_table[(event.type, event.key)]
+            self.add_event(key_event)
+
+
--- 2DGP 프로젝트/main_state.py
+++ 2DGP 프로젝트/main_state.py
@@ -50,76 +50,17 @@ def resume():
 
 
 def handle_events():
-    global cadence
-    global skeleton
-    global bat
-    global map
-    events=get_events()
+    events = get_events()
     for event in events:
-        if event.type==SDL_QUIT:
+        if event.type == SDL_QUIT:
             game_framework.quit()
-        elif event.type==SDL_KEYDOWN and event.key==SDLK_ESCAPE:
-            game_framework.change_state(title_state)
-
-        elif event.type==SDL_KEYDOWN and event.key==SDLK_LEFT:
-            if(stick.x>300):
-                if(cadence.x-24==bat.x and cadence.y==bat.y):
-                    bat.life-=1
-                    bat.x=0
-                    bat.y=0
-                    cadence.attack()
-
-                elif(cadence.x-24==skeleton.x and cadence.y==skeleton.y):
-                    skeleton.life-=1
-                    skeleton.x=0
-                    skeleton.y=0
-                else:
-                    cadence.x-=24
-
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_RIGHT:
-            if (stick.x > 300):
-                if (cadence.x + 24 == bat.x and cadence.y==bat.y):
-                    bat.life -= 1
-                    bat.x = 0
-                    bat.y = 0
-
-
-                elif (cadence.x + 24 == skeleton.x and cadence.y==skeleton.y):
-                    skeleton.life -= 1
-                    skeleton.x = 0
-                    skeleton.y = 0
-                else:
-                    cadence.x += 24
-
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_UP:
-            if (stick.x > 300):
-                if (cadence.y + 24 == bat.y and cadence.x == bat.x):
-                    bat.life -= 1
-                    bat.x = 0
-                    bat.y = 0
-                elif (cadence.y + 24 == skeleton.y and cadence.x== skeleton.x):
-                    skeleton.life -= 1
-                    skeleton.x = 0
-                    skeleton.y = 0
-                else:
-                    cadence.y += 24
-
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_DOWN:
-            if (stick.x > 300):
-                if (cadence.y - 24 == bat.y and cadence.x == bat.x):
-                    bat.life -= 1
-                    bat.x = 0
-                    bat.y = 0
-                elif (cadence.y - 24 == skeleton.y and cadence.x == skeleton.x):
-                    skeleton.life -= 1
-                    skeleton.x = 0
-                    skeleton.y = 0
-                else:
-                    cadence.y -= 24
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.quit()
+        else:
+            cadence.handle_event(event)
 
 
 def update():
-    global cadence, heart, stick, skeleton, bat, map
     heart.update()
     stick.update()
     cadence.update()
@@ -129,7 +70,6 @@ def update():
         bat.update()
 
 def draw():
-    global cadence, heart, stick, skeleton, bat, map
     clear_canvas()
     map.draw()
     cadence.draw()
